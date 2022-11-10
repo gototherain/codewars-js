5cf576dd9c2fb2917db2134418a68e973f580693
gototherain <go.to.the.rain@gmail.com>
2022-11-10 17:52:04 +0600

    08

---
+++ 08.js
@@ -0,0 +1,15 @@
+function findDifference(a, b) {
+  
+    var sum = a[0]*a[1]*a[2]-b[0]*b[1]*b[2];
+    
+    if (sum>0) {
+      
+      return sum;
+      
+    } else 
+      
+      return -sum;
+    
+    
+    //loading...
+  }
