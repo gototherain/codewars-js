8b4fa4ace826883c79d30e33bf33ebab4d6c1aea
gototherain <go.to.the.rain@gmail.com>
2022-11-11 12:47:45 +0600

    09

---
+++ 09.js
@@ -0,0 +1,10 @@
+function getAge(inputString){
+  
+    // return the girl's correct age as an integer. Happy coding :) 
+      const girlAge = parseInt(inputString);
+      return girlAge;
+     
+      
+    }
+    
+    
